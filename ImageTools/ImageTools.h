c6081d71aa5a0e73357c61e9e7d9cd897ff3242e
Tapasya K H S <tapasyakhs@gmail.com>
2012-09-03 10:35:32 +0530

    Initial Commit

---
+++ ImageTools/ImageTools.h
@@ -0,0 +1,13 @@
+//
+//  ImageTools.h
+//  ImageTools
+//
+//  Created by pramati on 9/3/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageTools : NSObject
+
+@end
---
+++ ImageTools/ImageTools.m
@@ -0,0 +1,13 @@
+//
+//  ImageTools.m
+//  ImageTools
+//
+//  Created by pramati on 9/3/12.
+//
+//
+
+#import "ImageTools.h"
+
+@implementation ImageTools
+
+@end
